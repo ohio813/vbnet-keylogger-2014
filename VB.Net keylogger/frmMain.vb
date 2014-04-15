8333097c5b0841d5a79aacbce0037c56955bed51
LoveYou <loveyou@brainsoft.gr>
2014-04-16 02:40:02 +0300

    Signed-off-by: LoveYou <loveyou@brainsoft.gr>

--- VB.Net keylogger/frmMain.vb
+++ VB.Net keylogger/frmMain.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmMain
     Private WithEvents WindowsHook As New Hook
+
+    Private Sub WindowsHook_KeyboardChange(nCode As Integer, wParam As Hook.WM_KEYBOARD_MSG, ByRef lParam As Hook.KBDLLHOOKSTRUCT, ByRef cancel As Boolean) Handles WindowsHook.KeyboardChange
+
+    End Sub
 End Class
